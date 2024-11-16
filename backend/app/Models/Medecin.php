b6015d9f1b9ae3d0a024cb8ebb44f49261b99441
oumaima <omeimataktak@gmail.com>
2024-11-16 18:34:21 +0100

    .env

--- backend/app/Models/Medecin.php
+++ backend/app/Models/Medecin.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 use Illuminate\Database\Eloquent\Model;
 
 class Medecin extends Model
+
+    //
+
+    
 {
+    use HasFactory;
+    protected $fillable=['nom','prenom','email','type'];
     //
 }
+
