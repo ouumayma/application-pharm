b4f5a9a18b131bb782a7a6dfb99c818f3b7404d7
oumaima <omeimataktak@gmail.com>
2024-12-02 13:06:07 +0100

    withAuth

--- backend/app/Http/Controllers/MedicamentController.php
+++ backend/app/Http/Controllers/MedicamentController.php
@@ -12,38 +12,70 @@ class MedicamentController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $medicaments = Medicament::all();
+            return response()->json($medicaments);
+        } catch (\Exception $e) {
+            return response()->json("probleme de récupération de la liste des medicaments");
+        }
     }
-
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
-    }
+        try {
+            $medicament = new Medicament([
+                "nomcategorie" => $request->input("nomcategorie"),
+                "imagecategorie" => $request->input("imagecategorie")
+            ]);
+            $medicament->save();
 
+
+            return response()->json($medicament);
+        } catch (\Exception $e) {
+            return response()->json("insertion impossible");
+        }
+    }
     /**
      * Display the specified resource.
      */
-    public function show(Medicament $medicament)
+    public function show($id)
     {
-        //
+        try {
+            $medicament = Medicament::findOrFail($id);
+            return response()->json($medicament);
+        } catch (\Exception $e) {
+            return response()->json("probleme de récupération des données");
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Medicament $medicament)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $medicament = medicament::findorFail($id);
+            $medicament->update($request->all());
+            return response()->json($medicament);
+        } catch (\Exception $e) {
+            return response()->json("probleme de modification");
+        }
     }
-
     /**
+    
+    
      * Remove the specified resource from storage.
      */
-    public function destroy(Medicament $medicament)
+    public function destroy($id)
     {
-        //
+        try {
+            $medicament = medicament::findOrFail($id);
+            $medicament->delete();
+            return response()->json("medicament supprimée avec succes");
+        } catch (\Exception $e) {
+            return response()->json("probleme de suppression de medicament");
+        }
     }
 }
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -1,28 +1,32 @@
 <?php
 
 namespace App\Models;
-
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+
+
+class User extends Authenticatable implements JWTSubject
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
-
+    use HasApiTokens, HasFactory, Notifiable;
     /**
      * The attributes that are mass assignable.
      *
+
      * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
+        'avatar',
+        'isActive'
     ];
-
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -32,17 +36,32 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to
+
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
